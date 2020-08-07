dc53b071e45b80c00de884e1a2f53578839ed559
Justin Suderman <jsuderman1@gmail.com>
2020-08-06 21:35:51 -0400

    added airline-routes.js

---
+++ routes/airline-routes.js
@@ -0,0 +1,52 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/airline", function(req, res) {
+    // Here we add an "include" property to our options in our findAll query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    db.airline.findAll({
+      include: [db.Post]
+    }).then(function(dbAirline) {
+      res.json(dbAirline);
+    });
+  });
+
+  app.get("/api/airline/:id", function(req, res) {
+    // Here we add an "include" property to our options in our findOne query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    db.airline.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Post]
+    }).then(function(dbairline) {
+      res.json(dbairline);
+    });
+  });
+
+  app.post("/api/index", function(req, res) {
+    db.airline.create(req.body).then(function(dbairline) {
+        db.airline.create({
+            airline: req.body.airline,
+            departureTime: req.body.departureTime,
+            boardingTime: req.body.boardingTime,
+            departureGate: req.body.departureGate,
+            seatAssignment: req.body.seatAssignment,
+        }).then(function(dbairline) {
+      res.json(dbairline);
+    });
+  });
+
+//   app.delete("/api/authors/:id", function(req, res) {
+//     db.Author.destroy({
+//       where: {
+//         id: req.params.id
+//       }
+//     }).then(function(dbAuthor) {
+//       res.json(dbAuthor);
+//     });
+//   });
+
+};
