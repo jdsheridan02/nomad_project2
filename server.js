55e4b3283abe531a0145ba1b4ae87a6fb666df35
jdsheridan02 <jdsheridan02@gmail.com>
2020-08-06 21:25:37 -0400

    created routes for hotel.js file for pulling and posting info and added routes to server file

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // Requiring necessary npm packages
-const express = require("express");
-const session = require("express-session");
-// Requiring passport as we've configured it
-const passport = require("./config/passport");
+const express = require("express"),
+      session = require("express-session"),
+  // Requiring passport as we've configured it
+      passport = require("./config/passport"),
+  // Set Handlebars.
+      exphbs = require("express-handlebars");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
@@ -17,12 +19,16 @@ app.use(express.static("public"));
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
+require("./routes/hotel-routes.js")(app);
+require("./routes/airline-routes.js")(app);
+
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
