9723ab99f41cb1a58bfeaabe63576283d184a6c2
Justin Suderman <jsuderman1@gmail.com>
2020-08-08 14:16:52 -0400

    fixed js errors

--- public/js/landing.js
+++ public/js/landing.js
@@ -24,6 +24,7 @@ $(document).ready(() => {
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
   function signUpUser(email, password) {
+    console.log("at the api landing")
     $.post("/api/landing", {
       email: email,
       password: password
