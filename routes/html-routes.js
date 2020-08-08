e20a21927775d5ccb0fb4cfe2b5c96b37ff3c19d
Justin Suderman <jsuderman1@gmail.com>
2020-08-08 14:18:50 -0400

    fixed pathways for app

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,18 +8,18 @@ module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/login");
+      res.redirect("/add");
     }
     res.render("./landing");
   });
 
-  app.get("/login", (req, res) => {
-    // If the user already has an account send them to the add page
-    // if (req.user) {
-    //   res.redirect("/login");
-    // }
-    res.render("./login");
-  });
+  // app.get("/login", (req, res) => {
+  //   // If the user already has an account send them to the add page
+  //   // if (req.user) {
+  //   //   res.redirect("/login");
+  //   // }
+  //   res.render("./login");
+  // });
 
   app.get("/add", (req, res) => {
     // If the user already has an account send them to the add page
@@ -39,7 +39,10 @@ module.exports = function(app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/login", isAuthenticated, (req, res) => {
-    res.render("./login");
+  app.get("/login", (req, res) => {
+    if (req.user) {
+      res.redirect("/add");
+    }
+    res.render("login");
   });
 };
