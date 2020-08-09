436cc8e157d1dab6884120269d5e7007b2a1d1c8
jdsheridan02 <jdsheridan02@gmail.com>
2020-08-09 12:38:07 -0400

    changed file names to reflect path of application

--- public/js/user.js
+++ public/js/user.js
@@ -1,32 +1,31 @@
-// Code here handles what happens when a user submits a new character on the form.
-// Effectively it takes the form inputs then sends it to the server to save in the DB.
+// Code here takes user input and post info into database to be recalled on index page.
 
 // when user clicks add-btn
 $("#airlineBtn").on("click", function(event) {
     event.preventDefault();
   
-    // make a newCharacter obj
+    // make a airline obj
     var airlineEntry = {
       // name from name input
       airline: $("#airline-name").val().trim(),
-      // role from role input
+      // departure time
       departureTime: $("#departure-time").val().trim(),
-      // age from age input
+      // boarding time
       boardingTime: $("#boarding-time").val().trim(),
-      // points from force-points input
+      // departure gate
       departureGate: $("#depature-gate").val().trim(),
-
+      // seat assignment
       seatAssignment: $("#seat-assignment").val().trim()
     };
   
     // send an AJAX POST-request with jQuery
-    $.post("/api/index", airlineEntry)
+    $.post("/api/user", airlineEntry)
       // on success, run this callback
       .then(function(data) {
         // log the data we found
         console.log(data);
-        // tell the user we're adding a character with an alert window
-        alert("Adding character...");
+        // tell the user we're adding hotel
+        console.log("Adding airline info...");
       });
   
     // // empty each input box by replacing the value with an empty string
@@ -40,26 +39,26 @@ $("#airlineBtn").on("click", function(event) {
   $("#hotelBtn").on("click", function(event) {
     event.preventDefault();
   
-    // make a newCharacter obj
+    // make a hotel obj
     var hotelEntry = {
       // name from name input
       name: $("#hotel-name").val().trim(),
-      // role from role input
+      // address info
       address: $("#hotel-address").val().trim(),
-      // age from age input
+      // check in date
       checkin: $("#check-in-date").val().trim(),
-      // points from force-points input
+      // checkout date
       checkout: $("#check-out-date").val().trim()
     };
   
     // send an AJAX POST-request with jQuery
-    $.post("/api/new", hotelEntry)
+    $.post("/api/user", hotelEntry)
       // on success, run this callback
       .then(function(data) {
         // log the data we found
         console.log(data);
         // tell the user we're adding a character with an alert window
-        alert("Adding character...");
+        console.log("Adding hotel info...");
       });
   
     // // empty each input box by replacing the value with an empty string
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,40 +8,40 @@ module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/add");
+      res.redirect("/user");
     }
-    res.render("./landing");
+    res.render("./signup");
   });
 
   // app.get("/login", (req, res) => {
-  //   // If the user already has an account send them to the add page
+  //   // If the user already has an account send them to the user page
   //   // if (req.user) {
   //   //   res.redirect("/login");
   //   // }
   //   res.render("./login");
   // });
 
-  app.get("/add", (req, res) => {
-    // If the user already has an account send them to the add page
+  app.get("/user", (req, res) => {
+    // If the user already has an account send them to the user page
     // if (req.user) {
-    //   res.redirect("/add");
+    //   res.redirect("/user");
     // }
-    res.render("./add");
+    res.render("./user");
   });
 
   app.get("/index", (req, res) => {
-    // If the user already has an account send them to the add page
+    // If the user already has an account send them to the user page
     // if (req.user) {
-    //   res.redirect("/add");
+    //   res.redirect("/user");
     // }
     res.render("./index");
   });
 
-  // Here we've add our isAuthenticated middleware to this route.
+  // Here we've user our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/login", (req, res) => {
     if (req.user) {
-      res.redirect("/add");
+      res.redirect("/user");
     }
     res.render("login");
   });
